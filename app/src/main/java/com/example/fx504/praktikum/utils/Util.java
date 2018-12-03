31626ef61b8a45333e00aeade33bf54e5238a404
FX504 <divarismawan.20@gmail.com>
2018-12-03 13:52:50 +0800

    showImage

--- app/src/main/java/com/example/fx504/praktikum/utils/Util.java
+++
@@ -1,6 +0,0 @@
-package com.example.fx504.praktikum.utils;
-
-public class Util {
-    public static String BASE_URL = "http://192.168.43.43:8000/";
-    public static String BASE_IMG_URL = BASE_URL + "StoryNovels/";
-}
